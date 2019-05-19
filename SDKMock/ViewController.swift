8dc76fab06f113501846548e1589558ec191b87c
近藤 寛志 <kondo@cloudpack.jp>
2019-05-19 20:10:31 +0900

    入力されたURLが有効な場合のみ開く

--- SDKMock/ViewController.swift
+++ SDKMock/ViewController.swift
@@ -14,7 +14,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+
     @IBAction func didEndOnExit(_ sender: UITextField) {
+        guard let urlString = sender.text, let url = URL(string: urlString) else {
+            return
+        }
+        
+        if UIApplication.shared.canOpenURL(url) {
+            // 入力したURLを開ける場合に開く
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        }
     }
     
 }
